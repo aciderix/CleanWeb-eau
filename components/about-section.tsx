37ad7a37a86d053a49c36c3b516d558ec934bb6a
aciderix <121115002+aciderix@users.noreply.github.com>
2025-04-03 22:44:26 +0200

    Push depuis l'interface d'administration CLEAN

--- components/about-section.tsx
+++ components/about-section.tsx
@@ -59,7 +59,7 @@ export default function AboutSection() {
             }}
           >
             <Image
-              src="/images/rive.jpeg"
+              src="/images/bateau.png"
               alt="Vue d'une rivière nantaise"
               fill
               className="object-cover transition-transform duration-500 group-hover:scale-105"
